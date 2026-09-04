sixfeetup/cloud-custodian

commit 1f20e9fa04064f1b0c8cadede893b41677597c0b

Wafv2 Refactor Scope Region Logic (#10827)
* cloudfront test fix

* draft wafv2 changes

* tagging fixes and tests

* docstring

* docstring

* revert linting

* client cache

* initialize _client

* static resource_type classes

* client lazy init

c7n/resources/waf.py
@@ -1,13 +1,16 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
+import functools
+
 from c7n.manager import resources
-from c7n.query import ConfigSource, QueryResourceManager, TypeInfo, DescribeSource, ResourceQuery
+from c7n.query import ConfigSource, QueryResourceManager, TypeInfo, DescribeSource
 from c7n.tags import universal_augment
 from c7n.filters import ValueFilter, ListItemFilter
 from c7n.utils import type_schema, local_session
 from c7n.actions import BaseAction
-from c7n.exceptions import PolicyValidationError
+from c7n.exceptions import ClientError, PolicyValidationError
 from c7n.resources.aws import shape_validate
+from c7n.utils import is_not_found
 
 
 class DescribeRegionalWaf(DescribeSource):
@@ -19,88 +22,6 @@ class DescribeRegionalWaf(DescribeSource):
     def augment(self, resources):
         resources = super().augment(resources)
         return universal_augment(self.manager, resources)
-
-
-class WafV2ResourceQuery(ResourceQuery):
-    """Custom query handler that uses us-east-1 for CLOUDFRONT scope WebACLs"""
-
-    def filter(self, resource_manager, **params):
-        """Query a set of resources, using us-east-1 for CLOUDFRONT scope."""
-        m = self.resolve(resource_manager.resource_type)
-
-        # CloudFront WebACLs must be queried from us-east-1
-        region = resource_manager.config.region
-        if params.get('Scope') == 'CLOUDFRONT':
-            region = 'us-east-1'
-
-        if resource_manager.get_client:
-            client = resource_manager.get_client()
-        else:
-            client = local_session(self.session_factory).client(m.service, region)
-
-        enum_op, path, extra_args = m.enum_spec
-        if extra_args:
-            params = {**extra_args, **params}
-        return (
-            self._invoke_client_enum(
-                client, enum_op, params, path, getattr(resource_manager, 'retry', None)
-            )
-            or []
-        )
-
-
-class DescribeWafV2(DescribeSource):
-    resource_query_factory = WafV2ResourceQuery
-
-    def get_permissions(self):
-        perms = super().get_permissions()
-        perms.remove('wafv2:GetWebAcl')
-        return perms
-
-    def augment(self, resources):
-        # CloudFront WebACLs (Scope=CLOUDFRONT) must be queried from us-east-1
-        region = self.manager.region
-        if resources and resources[0].get('Scope') == 'CLOUDFRONT':
-            region = 'us-east-1'
-
-        client = local_session(self.manager.session_factory).client('wafv2', region_name=region)
-
-        def _detail(webacl):
-            response = client.get_web_acl(
-                Name=webacl['Name'], Id=webacl['Id'], Scope=webacl['Scope']
-            )
-            detail = response.get('WebACL', {})
-
-            return {**webacl, **detail}
-
-        with_tags = universal_augment(self.manager, resources)
-
-        return list(map(_detail, with_tags))
-
-    # set REGIONAL for Scope as default
-    def get_query_params(self, query_params):
-        query_params = query_params or {}
-        # Parse query from policy data
-        queries = self.manager.data.get('query', [])
-        for q in queries:
-            query_params.update(q)
-        if 'Scope' not in query_params:
-            query_params['Scope'] = 'REGIONAL'
-        return query_params
-
-    def resources(self, query):
-        scope = (query or {}).get('Scope', 'REGIONAL')
-        # The AWS API does not include the scope as part of the WebACL information, but scope
-        # is a required parameter for most API calls - we augment the resource with the desired
-        # scope here in order to use it downstream for API calls
-        return [{'Scope': scope, **r} for r in super().resources(query)]
-
-    def get_resources(self, ids):
-        params = self.get_query_params(None)
-        scope = (params or {}).get('Scope', 'REGIONAL')
-
-        resources = self.query.filter(self.manager, **params)
-        return [{'Scope': scope, **r} for r in resources if r[self.manager.resource_type.id] in ids]
 
 
 class DescribeWaf(DescribeSource):
@@ -149,8 +70,83 @@ class RegionalWAF(QueryResourceManager):
     source_mapping = {'describe': DescribeRegionalWaf, 'config': ConfigSource}
 
 
+class DescribeWafV2(DescribeSource):
+
+    # Essentially a copy of DescribeSource.augment with the addition of Scope and Name parameters
+    def augment(self, resources):
+        client = self.manager.get_client()
+        scope = self.manager.scope
+        detail_op, param_name, param_key, detail_path = self.manager.resource_type.detail_spec
+
+        op = getattr(client, detail_op)
+        if self.manager.retry:
+            op = functools.partial(self.manager.retry, op)
+
+        def get_detail(r):
+            kwargs = {
+                param_name: r[param_key],
+                'Scope': scope,
+                'Name': r['Name']
+            }
+            try:
+                response = op(**kwargs)
+            except ClientError as e:
+                if not is_not_found(e):
+                    raise
+                self.manager.log.warning("Resource not found: %s id:%s" % (detail_op, r[param_key]))
+                return None
+            r.update(response.get(detail_path, {}))
+            r['Scope'] = scope
+            return r
+
+        resources = universal_augment(self.manager, resources)
+        return [r for r in map(get_detail, resources) if r is not None]
+
+    def get_resources(self, ids):
+        params = {'Scope': self.manager.scope}
+        resources = self.query.filter(self.manager, **params)
+        id_key = self.manager.resource_type.id
+        return [r for r in resources if r[id_key] in ids]
+
+    def get_permissions(self):
+        perms = super().get_permissions()
+        perms.remove('wafv2:GetWebAcl')
+        return perms
+
+    # set REGIONAL for Scope as default
+    def get_query_params(self, query_params):
+        query_params = query_params or {}
+        # Parse query from policy data
+        queries = self.manager.data.get('query', [])
+        for q in queries:
+            query_params.update(q)
+        query_params['Scope'] = self.manager.scope
+        return query_params
+
+
 @resources.register('wafv2')
 class WAFV2(QueryResourceManager):
+    """WAFv2 Web ACLs.
+
+    By default, queries REGIONAL scope Web ACLs. To query CloudFront (global)
+    Web ACLs, set the scope in the query block:
+
+    .. code-block:: yaml
+
+        policies:
+          - name: cloudfront-webacls
+            resource: aws.wafv2
+            query:
+              - Scope: CLOUDFRONT
+
+    CLOUDFRONT-scoped Web ACLs are global resources managed via us-east-1
+    regardless of the policy's configured region.
+    """
+
+    CLOUDFRONT = "CLOUDFRONT"
+    REGIONAL = "REGIONAL"
+    VALID_SCOPES = (CLOUDFRONT, REGIONAL)
+
     class resource_type(TypeInfo):
         service = "wafv2"
         enum_spec = ("list_web_acls", "WebACLs", None)
@@ -165,8 +161,48 @@ class WAFV2(QueryResourceManager):
         permissions_enum = ('wafv2:ListWebACLs',)
         permissions_augment = ('wafv2:GetWebACL', "wafv2:ListTagsForResource")
         universal_taggable = object()
+        global_resource = False
+
+    class cloudfront_resource_type(resource_type):
+        global_resource = True
 
     source_mapping = {'describe': DescribeWafV2, 'config': ConfigSource}
+
+    def __init__(self, ctx, data):
+        super().__init__(ctx, data)
+        self._client = None
+        if self.scope == self.CLOUDFRONT:
+            self.resource_type = self.cloudfront_resource_type
+
+    def validate(self):
+        for q in self.data.get('query', []):
+            if 'Scope' in q and q['Scope'] not in self.VALID_SCOPES:
+                raise PolicyValidationError(
+                    f"Invalid Scope: {q['Scope']}.  Must be one of {self.VALID_SCOPES}"
+                )
+
+    @property
+    def scope(self):
+        for q in self.data.get('query', []):
+            if 'Scope' in q:
+                return q['Scope']
+        return self.REGIONAL
+
+    @property
+    def scope_region(self):
+        """Region to use for wafv2 API calls given a WebACL's Scope.
+
+        CLOUDFRONT WebACLs are global resources addressed via us-east-1; all other
+        (REGIONAL) WebACLs use the policy's region.
+        """
+        return 'us-east-1' if self.scope == self.CLOUDFRONT else self.region
+
+    def get_client(self):
+        if self._client is None:
+            self._client = local_session(self.session_factory).client(
+                self.resource_type.service, region_name=self.scope_region)
+
+        return self._client
 
 
 @WAFV2.filter_registry.register('logging')
@@ -202,9 +238,7 @@ class WAFV2LoggingFilter(ValueFilter):
     annotation_key = 'c7n:WafV2LoggingConfiguration'
 
     def process(self, resources, event=None):
-        client = local_session(self.manager.session_factory).client(
-            'wafv2', region_name=self.manager.region
-        )
+        client = self.manager.get_client()
         logging_confs = client.list_logging_configurations(Scope='REGIONAL')[
             'LoggingConfigurations'
         ]
@@ -292,9 +326,7 @@ class WAFV2SetLogging(BaseAction):
         return self
 
     def process(self, resources):
-        client = local_session(self.manager.session_factory).client(
-            'wafv2', region_name=self.manager.region
-        )
+        client = self.manager.get_client()
         destination = self.data['destination']
         attributes = self.data.get('attributes', {})
 
@@ -410,9 +442,7 @@ class WAFV2ListAllRulesFilter(ListItemFilter):
         return mgcache
 
     def get_item_values(self, resource):
-        client = local_session(self.manager.session_factory).client(
-            'wafv2', region_name=self.manager.region
-        )
+        client = self.manager.get_client()
 
         rule_groups = []
         managed_groups = []

tests/test_cloudfront.py
@@ -1099,82 +1099,35 @@ class CloudFrontWafV2(BaseTest):
         self.assertEqual(len(resources), 1)
 
     def test_wafv2_cloudfront_scope_uses_us_east_1(self):
-        """Test that WAFv2 queries with CLOUDFRONT scope use us-east-1 region.
+        """Regression test: CLOUDFRONT scope must use us-east-1 via WAFV2.get_client().
 
-        This is a regression test for the bug where CLOUDFRONT scope queries
-        would use the policy's region instead of us-east-1, causing:
-        WAFInvalidParameterException: The scope is not valid., field: SCOPE_VALUE
-
-        This test directly verifies the WafV2ResourceQuery behavior.
+        Previously WAFInvalidParameterException would be raised when a policy
+        configured in a non-us-east-1 region queried CLOUDFRONT-scoped WebACLs.
         """
-        from c7n.resources.waf import WafV2ResourceQuery
-        from c7n.query import TypeInfo
+        from c7n.config import Config
+        from c7n.resources.waf import WAFV2
 
-        # Create a real TypeInfo class for WAFv2
-        class MockResourceType(TypeInfo):
-            service = 'wafv2'
-            enum_spec = ('list_web_acls', 'WebACLs', None)
-
-        # Create a mock resource manager with config.region = 'us-west-2'
-        mock_manager = MagicMock()
-        mock_manager.config.region = 'us-west-2'
-        mock_manager.get_client = None  # Force creation of new client
-        mock_manager.resource_type = MockResourceType
-        mock_manager.retry = None
-
-        # Create the query handler
-        query = WafV2ResourceQuery(lambda: MagicMock())
-
-        # Mock local_session to track what region is used
         with patch('c7n.resources.waf.local_session') as mock_local_session:
             mock_session = MagicMock()
-            mock_client = MagicMock()
-            mock_client.list_web_acls.return_value = {'WebACLs': []}
-            mock_client.can_paginate.return_value = False
-            mock_session.client.return_value = mock_client
             mock_local_session.return_value = mock_session
 
-            # Test 1: Query with CLOUDFRONT scope should use us-east-1
-            query.filter(mock_manager, Scope='CLOUDFRONT')
+            ctx = self.get_context(config=Config.empty(region='us-west-2'))
 
-            # Verify client was created with us-east-1, not us-west-2
-            mock_session.client.assert_called()
-            call_args = mock_session.client.call_args
-            self.assertEqual(call_args[0][0], 'wafv2', "Should create wafv2 client")
-            self.assertEqual(
-                call_args[0][1],
-                'us-east-1',
-                "Should use us-east-1 for CLOUDFRONT scope, not us-west-2",
-            )
+            # CLOUDFRONT scope should always use us-east-1
+            manager = WAFV2(ctx, {'query': [{'Scope': 'CLOUDFRONT'}]})
+            manager.get_client()
+            mock_session.client.assert_called_once_with('wafv2', region_name='us-east-1')
 
-            # Reset mock
             mock_session.client.reset_mock()
 
-            # Test 2: Query with REGIONAL scope should use manager's region
-            query.filter(mock_manager, Scope='REGIONAL')
+            # REGIONAL scope should use the configured region
+            manager = WAFV2(ctx, {'query': [{'Scope': 'REGIONAL'}]})
+            manager.get_client()
+            mock_session.client.assert_called_once_with('wafv2', region_name='us-west-2')
 
-            # Verify client was created with us-west-2
-            mock_session.client.assert_called()
-            call_args = mock_session.client.call_args
-            self.assertEqual(call_args[0][0], 'wafv2', "Should create wafv2 client")
-            self.assertEqual(
-                call_args[0][1],
-                'us-west-2',
-                "Should use manager's region (us-west-2) for REGIONAL scope",
-            )
-
-            # Reset mock
             mock_session.client.reset_mock()
 
-            # Test 2: Query with REGIONAL scope should use manager's region
-            query.filter(mock_manager, Scope='REGIONAL')
-
-            # Verify client was created with us-west-2
-            mock_session.client.assert_called()
-            call_args = mock_session.client.call_args
-            self.assertEqual(call_args[0][0], 'wafv2', "Should create wafv2 client")
-            self.assertEqual(
-                call_args[0][1],
-                'us-west-2',
-                "Should use manager's region (us-west-2) for REGIONAL scope",
-            )
+            # No scope in query should default to REGIONAL
+            manager = WAFV2(ctx, {})
+            manager.get_client()
+            mock_session.client.assert_called_once_with('wafv2', region_name='us-west-2')

stacklet/cloud-custodian-prerelease

commit fc8d0ac72e8e2c886468ad2fc2a34e5ce22978d7

fix: aws - cloudfront - wafv2-enabled scope, stamp .post1 (#22)
[ENG-8730](https://stacklet.atlassian.net/browse/ENG-8730)

### what

Cherry-pick upstream
[#11039](https://github.com/cloud-custodian/cloud-custodian/pull/11039)
onto the 2026.08.26 release branch and stamp the workspace
`0.9.52a5.post1+a83dda5c9`.

`WafV2FilterBase._get_web_acls` passed the scope it wanted through
`resources(query=...)`, which the wafv2 resource manager then
overwrote with its own. It now passes the scope as resource manager
data, where the manager reads both the API `Scope` and the region to
call from. `DescribeWafV2.get_query_params` raises
`PolicyValidationError` on a conflicting caller-supplied `Scope`
rather than discarding it, so the same mistake fails loudly.

The release workflow's `Assert prerelease version` guard accepts an
optional `.postM` between `aN` and `+HASH`. It still rejects a
version missing either the prerelease or the local segment, and a
`.post` that is not followed by digits.

### why

CLOUDFRONT web ACLs are global and are only listed from us-east-1.
With the requested scope dropped in favour of the REGIONAL default,
`wafv2-enabled` never saw a distribution's web ACL, so
`state: false` matched distributions that are in fact protected —
false positives on `aws-cloudfront-waf-disabled-inform` for a
customer running that policy.

2026.08.26 shipped
[#10975](https://github.com/cloud-custodian/cloud-custodian/pull/10975),
which fixed the scope for the `set-wafv2` *action*, but the filter
regression was merged upstream after the cohort's c7n was stamped.

Fixes: 1f20e9fa04064f1b0c8cadede893b41677597c0b ("Wafv2 Refactor Scope
Region Logic (#10827)")

### testing

The cherry-pick brings upstream's coverage: a distribution with a
CLOUDFRONT-scoped web ACL attached is not matched by
`wafv2-enabled` with `state: false`, and the filter is asserted to
have listed web ACLs with `Scope: CLOUDFRONT`. A second test covers

.github/workflows/stacklet-release.yml
@@ -26,15 +26,16 @@ jobs:
           persist-credentials: false
 
       # This repo only ever ships prereleases stamped with a +HASH local
-      # version segment (…aN+HASH). A version without both is a misconfiguration
+      # version segment (…aN+HASH, or …aN.postM+HASH for a hotfix on a
+      # release branch). A version without both is a misconfiguration
       # — fail loudly before building or publishing anything.
       - name: Assert prerelease version
         shell: bash
         run: |
           set -euo pipefail
           version=$(grep -m1 '^version' pyproject.toml | cut -d'"' -f2)
-          if [[ ! "$version" =~ a[0-9]+[+][0-9a-zA-Z.]+$ ]]; then
-            echo "::error::cloud-custodian-prerelease must ship a stamped prerelease version (…aN+HASH); got '$version'"
+          if [[ ! "$version" =~ a[0-9]+(\.post[0-9]+)?[+][0-9a-zA-Z.]+$ ]]; then
+            echo "::error::cloud-custodian-prerelease must ship a stamped prerelease version (…aN+HASH or …aN.postM+HASH); got '$version'"
             exit 1
           fi
           echo "Releasing prerelease version $version"

c7n/filters/waf.py
@@ -223,8 +223,14 @@ class WafV2FilterBase(ValueFilter, metaclass=ABCMeta):
     # based on the scope
     def _get_web_acls(self, scope):
         if self._cached_web_acls is None:
-            self._cached_web_acls = self.manager.get_resource_manager('wafv2').resources(
-                query=dict(Scope=scope),
+            # Scope must be supplied as resource manager data rather than a query
+            # kwarg. The wafv2 manager derives both the API Scope and the region
+            # used for the call from its own data (CLOUDFRONT web acls are global
+            # and must be addressed via us-east-1), and a Scope passed via
+            # resources(query=...) is overwritten by the manager's own value.
+            wafv2 = self.manager.get_resource_manager(
+                'wafv2', {'query': [{'Scope': scope}]})
+            self._cached_web_acls = wafv2.resources(
                 # required to get the additional detail needed for this filter (e.g. Rules), but
                 # the legacy mode does not require additional detail
                 augment=(not self._is_legacy)

c7n/resources/waf.py
@@ -249,6 +249,15 @@ class DescribeWafV2(DescribeSource):
     # set REGIONAL for Scope as default
     def get_query_params(self, query_params):
         query_params = query_params or {}
+        # Scope is owned by the resource manager, since it also determines which
+        # region the wafv2 API is called in. Fail loudly rather than silently
+        # discarding a conflicting Scope supplied by the caller.
+        requested_scope = query_params.get('Scope')
+        if requested_scope is not None and requested_scope != self.manager.scope:
+            raise PolicyValidationError(
+                "wafv2 Scope conflict: %s was requested but the resource manager "
+                "scope is %s. Pass Scope via the resource manager's 'query' data."
+                % (requested_scope, self.manager.scope))
         # Parse query from policy data
         queries = self.manager.data.get('query', [])
         for q in queries:

c7n/version.py
@@ -1,2 +1,2 @@
 # Generated via tools/dev/devpkg.py
-version = "0.9.52a5+a83dda5c9"
+version = "0.9.52a5.post1+a83dda5c9"

pyproject.toml
@@ -16,7 +16,7 @@ dependencies = [
     "urllib3>2",
 ]
 name = "c7n"
-version = "0.9.52a5+a83dda5c9"
+version = "0.9.52a5.post1+a83dda5c9"
 description = "Cloud Custodian - Policy Rules Engine"
 readme = "README.md"
 classifiers = [

tools/c7n_awscc/pyproject.toml
@@ -10,7 +10,7 @@ dependencies = [
     "jsonpatch<2.0,>=1.32",
 ]
 name = "c7n_awscc"
-version = "0.1.37a5+a83dda5c9"
+version = "0.1.37a5.post1+a83dda5c9"
 readme = "readme.md"
 description = "Cloud Custodian - AWS Cloud Control Provider"
 classifiers = [

tools/c7n_azure/pyproject.toml
@@ -87,7 +87,7 @@ dependencies = [
     "requests<3.0.0,>=2.22.0",
 ]
 name = "c7n_azure"
-version = "0.7.51a5+a83dda5c9"
+version = "0.7.51a5.post1+a83dda5c9"
 description = "Cloud Custodian - Azure Support"
 readme = "readme.md"
 classifiers = [

tools/c7n_gcp/pyproject.toml
@@ -15,7 +15,7 @@ dependencies = [
     "retrying<2.0.0,>=1.3.3",
 ]
 name = "c7n_gcp"
-version = "0.4.51a5+a83dda5c9"
+version = "0.4.51a5.post1+a83dda5c9"
 description = "Cloud Custodian - Google Cloud Provider"
 readme = "readme.md"
 classifiers = [

tools/c7n_kube/pyproject.toml
@@ -10,7 +10,7 @@ dependencies = [
     "kubernetes>=10.0.1",
 ]
 name = "c7n_kube"
-version = "0.2.51a5+a83dda5c9"
+version = "0.2.51a5.post1+a83dda5c9"
 description = "Cloud Custodian - Kubernetes Provider"
 readme = "readme.md"
 classifiers = [

tools/c7n_left/pyproject.toml
@@ -12,7 +12,7 @@ dependencies = [
     "tfparse<1.0,>=0.6",
 ]
 name = "c7n_left"
-version = "0.3.38a5+a83dda5c9"
+version = "0.3.38a5.post1+a83dda5c9"
 description = "Custodian policies for IAAC definitions"
 readme = "README.md"
 classifiers = [

tools/c7n_mailer/pyproject.toml
@@ -20,7 +20,7 @@ dependencies = [
     "types-six<2.0.0,>=1.16.10",
 ]
 name = "c7n_mailer"
-version = "0.6.51a5+a83dda5c9"
+version = "0.6.51a5.post1+a83dda5c9"
 description = "Cloud Custodian - Reference Mailer"
 readme = "README.md"
 classifiers = [

tools/c7n_oci/pyproject.toml
@@ -13,7 +13,7 @@ dependencies = [
     "pyopenssl>=24"
 ]
 name = "c7n_oci"
-version = "0.1.26a5+a83dda5c9"
+version = "0.1.26a5.post1+a83dda5c9"
 description = "Cloud Custodian - OCI Support"
 readme = "readme.md"
 classifiers = [

tools/c7n_openstack/pyproject.toml
@@ -9,7 +9,7 @@ dependencies = [
     "openstacksdk==0.52.0",
 ]
 name = "c7n_openstack"
-version = "0.1.42a5+a83dda5c9"
+version = "0.1.42a5.post1+a83dda5c9"
 description = "Cloud Custodian - OpenStack Provider"
 readme = "readme.md"
 classifiers = [

tools/c7n_org/pyproject.toml
@@ -9,7 +9,7 @@ dependencies = [
     "click>=8.0",
 ]
 name = "c7n_org"
-version = "0.6.51a5+a83dda5c9"
+version = "0.6.51a5.post1+a83dda5c9"
 description = "Cloud Custodian - Parallel Execution"
 readme = "README.md"
 classifiers = [

tools/c7n_policystream/pyproject.toml
@@ -13,7 +13,7 @@ dependencies = [
     "requests<3.0.0,>=2.22.0",
 ]
 name = "c7n_policystream"
-version = "0.4.51a5+a83dda5c9"
+version = "0.4.51a5.post1+a83dda5c9"
 description = "Cloud Custodian - Git Commits as Logical Policy Changes"
 readme = "README.md"
 classifiers = [

tools/c7n_sphinxext/pyproject.toml
@@ -15,7 +15,7 @@ dependencies = [
     "sphinx-rtd-theme>=1.0.0",
 ]
 name = "c7n_sphinxext"
-version = "1.1.51a5+a83dda5c9"
+version = "1.1.51a5.post1+a83dda5c9"
 description = "Cloud Custodian - Sphinx Extensions"
 classifiers = [
     "License :: OSI Approved :: Apache Software License",

tools/c7n_tencentcloud/pyproject.toml
@@ -12,7 +12,7 @@ dependencies = [
     "tencentcloud-sdk-python<4.0.0,>=3.0.783",
 ]
 name = "c7n_tencentcloud"
-version = "0.1.34a5+a83dda5c9"
+version = "0.1.34a5.post1+a83dda5c9"
 description = "Cloud Custodian - Tencent Cloud Provider"
 readme = "readme.md"
 

tests/test_cloudfront.py
@@ -1,7 +1,9 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
 from .common import BaseTest, event_data
+from c7n.exceptions import PolicyValidationError
 from c7n.resources.aws import shape_validate
+from c7n.resources.waf import WAFV2, DescribeWafV2
 from c7n.utils import local_session, jmespath_compile
 from unittest.mock import MagicMock, patch
 from botocore.exceptions import ClientError
@@ -1157,3 +1159,43 @@ class CloudFrontWafV2(BaseTest):
             manager = WAFV2(ctx, {})
             manager.get_client()
             mock_session.client.assert_called_once_with('wafv2', region_name='us-west-2')
+
+    def test_wafv2_filter_uses_cloudfront_scope(self):
+        """The wafv2-enabled filter must list CLOUDFRONT-scoped web acls.
+
+        A distribution with a CLOUDFRONT WebACL attached must not be reported by
+        `state: false`. If the filter's requested scope is dropped in favor of the
+        REGIONAL default, the distribution's CLOUDFRONT WebACL ARN is never found
+        and the distribution is incorrectly reported as unprotected.
+        """
+        factory = self.replay_flight_data("test_distribution_wafv2_filter_scope")
+        p = self.load_policy(
+            {
+                "name": "wafv2-cloudfront-scope",
+                "resource": "distribution",
+                "filters": [{"type": "wafv2-enabled", "state": False}],
+            },
+            session_factory=factory,
+        )
+
+        scopes = []
+        original = DescribeWafV2.get_query_params
+
+        def record(self, query):
+            params = original(self, query)
+            scopes.append(params.get('Scope'))
+            return params
+
+        with patch.object(DescribeWafV2, 'get_query_params', record):
+            resources = p.run()
+
+        self.assertEqual(scopes, ['CLOUDFRONT'])
+        self.assertEqual(len(resources), 0)
+
+    def test_wafv2_filter_scope_conflict_raises(self):
+        """A Scope passed via resources(query=) that conflicts with the resource
+        manager's own scope is an error rather than being silently ignored."""
+        p = self.load_policy({"name": "wafv2", "resource": "distribution"})
+        mgr = WAFV2(p.ctx, {'query': [{'Scope': 'REGIONAL'}]})
+        with self.assertRaises(PolicyValidationError):
+            mgr.source.get_query_params({'Scope': 'CLOUDFRONT'})